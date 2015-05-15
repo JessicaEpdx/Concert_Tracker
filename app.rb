1ef9e6196e91ede53344f5f5174a0ab3c1f995ec
Jessica Engel <Jessicaepdx@gmail.com>
2015-05-15 09:48:25 -0700

    add band, see all bands

--- app.rb
+++ app.rb
@@ -1,3 +1,25 @@
 require("bundler/setup")
 Bundler.require(:default, :production)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  erb(:index)
+end
+
+get('/bands/form') do
+  erb(:bands_form)
+end
+
+get('/bands') do
+  @all_bands = Band.all()
+  erb(:bands)
+end
+
+post('/bands/form') do
+  new_band = Band.new({:name => params.fetch("name")})
+  if new_band.save
+    redirect('/bands')
+  else
+    erb(:error)
+  end
+end
